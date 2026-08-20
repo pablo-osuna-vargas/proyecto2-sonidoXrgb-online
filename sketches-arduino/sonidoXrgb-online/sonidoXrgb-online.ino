01013af5b05b439396cb0bfe9816d84566a24f93
pablo-osuna-vargas <pabloosunavargas@gmail.com>
2026-08-19 22:37:31 -0300

    Update sonidoXrgb-online.ino

--- sketches-arduino/sonidoXrgb-online/sonidoXrgb-online.ino
+++ sketches-arduino/sonidoXrgb-online/sonidoXrgb-online.ino
@@ -10,8 +10,8 @@ void loop() {
 
   for(int i = 0; i < 3; i++) {
 
-    int lectura1 = map(analogRead(pinPot0), 0, 1023, 20, 255);
-    int lectura2 = map(analogRead(pinPot1), 0, 1023, 20, 255);
+    int lectura1 = map(analogRead(pinPot0), 0, 1023, 0, 255);
+    int lectura2 = map(analogRead(pinPot1), 0, 1023, 0, 255);
     int lectura3 = map(analogRead(pinPot2), 0, 1023, 0, 255);
 
     Serial.print(lectura1);
