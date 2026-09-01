f3f63c5008df1b8405474f9fcb1696bc3956b5bf
pablo-osuna-vargas <pabloosunavargas@gmail.com>
2026-09-01 12:42:06 -0300

    Update sonidoXrgb-online.ino

--- sketches-arduino/sonidoXrgb-online/sonidoXrgb-online.ino
+++ sketches-arduino/sonidoXrgb-online/sonidoXrgb-online.ino
@@ -1,25 +1,9 @@
+int ledPin = 9; // pin PWM
+
 void setup() {
-  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-
-  int pinPot0 = 0;
-  int pinPot1 = 1;
-  int pinPot2 = 2;
-
-  for(int i = 0; i < 3; i++) {
-
-    int lectura1 = map(analogRead(pinPot0), 0, 1023, 0, 255);
-    int lectura2 = map(analogRead(pinPot1), 0, 1023, 0, 255);
-    int lectura3 = map(analogRead(pinPot2), 0, 1023, 0, 255);
-
-    Serial.print(lectura1);
-    Serial.print(",");
-    Serial.print(lectura2);
-    Serial.print(",");
-    Serial.println(lectura3);
-
-  }
-  delay(10);
+  analogWrite(ledPin, 200); // duty cycle 50% → ~2.5 V promedio
 }
